84e9c65fe72ebd93b42a42ba77a56fa09632ec01
Mia <miacrandell@gmail.com>
2018-04-10 14:32:20 -0600

    basic navbar, need different logo image

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,40 +1,21 @@
 import React from "react";
 import "./Nav.css";
 import Logo from "./images/logo-transp.png";
-import { Navbar, NavItem, Dropdown, Button } from "react-materialize";
+import { Navbar, NavItem, Dropdown, Col } from "react-materialize";
 
 const Nav = props => (
     <Navbar>
         <img src={Logo} className="logo brand-logo center"></img>
-        <Dropdown trigger={
-            <i className="material-icons">menu</i>
+        <Dropdown 
+        className="hamburger"
+        trigger={
+            <i className="material-icons md-48 menu">menu</i>
         }>
-            <NavItem>one</NavItem>
-            <NavItem>two</NavItem>
-            <NavItem divider />
-            <NavItem>three</NavItem>
+            <NavItem className="nav-item"><span className="nav-item-text">Dashboard</span></NavItem>
+            <NavItem className="nav-item"><span className="nav-item-text">Add Budget</span></NavItem>
+            <NavItem className="nav-item"><span className="nav-item-text">Logout</span></NavItem>
         </Dropdown>
     </Navbar>
-    // <nav>
-    //     <div className="nav-wrapper">
-    //         <ul id="dropdown1" class="dropdown-content">
-    //             <li><a href="#" data-activates="mobile-demo" className="button-collapse dropdown-trigger"><i className="material-icons">menu</i></a></li>
-    //             <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">Dropdown<i class="material-icons right">arrow_drop_down</i></a></li>
-    //             <li><a href="#!">one</a></li>
-    //             <li><a href="#!">two</a></li>
-    //             <li class="divider"></li>
-    //             <li><a href="#!">three</a></li>
-    //         </ul>
-    //         <img src={Logo} className="logo brand-logo center"></img>
-    //         <ul id="nav-mobile" className="left hide-on-med-and-down">
-    //             <li><a href="sass.html">Sass</a></li>
-    //             <li><a href="badges.html">Components</a></li>
-    //             <li><a href="collapsible.html">JavaScript</a></li>
-    //             <li><a href="#" data-activates="mobile-demo" className="button-collapse dropdown-trigger"><i className="material-icons">menu</i></a></li>
-    //             <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">Dropdown<i className="material-icons">menu</i></a></li>
-    //         </ul>
-    //     </div>
-    // </nav>
 );
 
 export default Nav;
