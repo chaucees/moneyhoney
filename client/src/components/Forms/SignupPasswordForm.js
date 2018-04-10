be562c5832ab975f8c16eac890a1dea4a3f1136d
Mia <miacrandell@gmail.com>
2018-04-10 12:31:22 -0600

    add password part of signup page

--- client/src/components/Forms/SignupPasswordForm.js
+++ client/src/components/Forms/SignupPasswordForm.js
@@ -2,5 +2,22 @@ import React from "react";
 import "./Forms.css";
 
 export const SignupPasswordForm = props => (
-    <p>Signup with Password goes here </p>
+    <div className="row">
+        <div className="input-field col s8 offset-s2">
+            <input
+                type="text"
+                id="password"
+                {...props}
+            />
+            <label htmlFor="password">Password</label>
+        </div>
+        <div className="input-field col s8 offset-s2">
+            <input
+                type="text"
+                id="retype-password"
+                {...props}
+            />
+            <label htmlFor="retype-password">Re-Type Password</label>
+        </div>
+    </div>
 );
