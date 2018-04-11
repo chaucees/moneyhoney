d38bbfb2f1b46c7d0723e63b022a12793126d30b
Chaucee <hi@chaucee.com>
2018-04-11 10:47:31 -0600

    Signup Page styling

--- client/src/pages/Signup/Signup.js
+++ client/src/pages/Signup/Signup.js
@@ -5,7 +5,23 @@ class Signup extends Component {
 
     render() {
         return (
-           <p>Signup Page</p> 
+            <div className="container-fluid">
+                <div className="section homeheader">
+                    <div className="col s12 m8 l8">
+                        <p className="header-text">Prepare For The Future</p>
+                        <p className="header-text">You Deserve</p>
+                        <p className="sub-header-text">Money Management Made Easy</p>
+                    </div>
+                </div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col s12">
+                            <h1>Form Goes HEre</h1>
+                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae molestiae similique error sit quo dicta vitae eos, ad doloribus alias. Natus accusamus rerum aut eaque excepturi quibusdam quidem, laborum, nisi neque iste numquam dolorem quae distinctio. Est, quis at, doloribus obcaecati autem adipisci vero a fuga recusandae, sunt laboriosam beatae?</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
