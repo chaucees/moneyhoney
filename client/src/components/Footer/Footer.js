cc80d28427f0af3b1429b38a568145046dd5158a
Mia <miacrandell@gmail.com>
2018-04-21 12:00:56 -0600

    clean up look of footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,34 +1,21 @@
 import React from "react";
 import "./Footer.css";
-import { Footer } from "react-materialize";
+// import { Footer } from "react-materialize";
 import Icons from "./images/socialicons.png";
 
 const FooterBar = props => (
-    <Footer copyrights="Designed by The Final Four"
-        moreLinks={
-            <a className="grey-text text-lighten-4 right" href="#!">&copy; 2018</a>
-        }>
-    <div className="container-fluid">
-        <div className="row">
-            <div className="col s12 m6">
-                <h5 className="white-text">Follow</h5>
-                <p className="grey-text text-lighten-4"><img src={Icons} alt="icons"></img></p>
-            </div>
-            <div className="col s12 m6">
-                <h5 className="white-text">Account</h5>
-                <ul>
-                    <li><a className="grey-text text-lighten-3" href="#">Login</a></li>
-                    <li><a className="grey-text text-lighten-3" href="dashboard">Profile</a></li>
-                    <li><a className="grey-text text-lighten-3" href="logout">Logout</a></li>
-                    <li><a className="grey-text text-lighten-3" href="signup">Sign Up</a></li>
-                </ul>
-            </div>
+
+    <footer class="page-footer">
+        <div class="col l6 s12">
+            {/* <h5 className="white-text center">Follow</h5> */}
+            <p className="grey-text text-lighten-4 icons"><img src={Icons} alt="icons"></img></p>
         </div>
-    </div>
+        <div class="footer-copyright">
+            <p className="designed-by">Designed by the Final Four</p>
+            <a className="grey-text text-lighten-4 right copyright" href="#!">&copy; 2018</a>
+        </div>
+    </footer>
 
-    
-
-    </Footer>
 );
 
 export default FooterBar;
