3d3a9f27e739b354f73fd76d35a5756821e698a1
Chaucee <hi@chaucee.com>
2018-04-21 10:18:44 -0600

    sign up page is mobile res now

--- client/src/pages/Signup/Signup.js
+++ client/src/pages/Signup/Signup.js
@@ -7,21 +7,15 @@ class Signup extends Component {
 
     render() {
         return (
-            <div className="container-fluid">
-                <div className="row body-bg-sign-up valign-wrapper">
-                    <div className="col s12">
-                    <center>
-                    <h1>Sign Up</h1>
-                    <p><i>Start Saving Today!</i></p>
-                    </center>
-                    </div>
-                </div>
+            <div className="container-fluid body-bg-sign-up">
                 <div class="row">
-                    <div className="container section2-signup">
+                    <div className="container">
                         <div className="row">
-                            <div className="col s12">
-                            <center><h1>Step One.</h1></center>
+                            <div className="col l6 m8 s12 offset-l3 offset-m2 section2-signup">
+                            <center><h1>Sign Up</h1>
+                            <p><i>and start saving today!</i></p></center>
                                 <SignupNameForm />
+                                <p></p>
                             </div>
                         </div>
                 </div>
