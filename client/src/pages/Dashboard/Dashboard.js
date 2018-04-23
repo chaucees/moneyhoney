35089c4cb0a0b734c1af3e5a20156c987c695d76
Mia <miacrandell@gmail.com>
2018-04-23 12:24:43 -0600

    format code on dashboard.js

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,65 +1,65 @@
 import React, { Component } from 'react';
 import "./Dashboard.css";
-import { ViewBtn, AddBtnAlt } from "../../components/Buttons";
+import { ViewBtn, AddBtnAlt, DeleteBtn } from "../../components/Buttons";
 import { DashPanel, DashPanelName } from "../../components/DashPanel";
 import { ProfileName } from "../../components/Profile";
 
 class Dashboard extends Component {
-    componentWillMount() {
-        this.setState({ profile: {} });
-        const { userProfile, getProfile } = this.props.auth;
-        if (!userProfile) {
-            getProfile((err, profile) => {
-                this.setState({ profile: profile });
-            });
-        } else {
-            this.setState({ profile: userProfile });
-        }
-    }
-    render() {
-        const { profile, email } = this.state;
-        return (
-<div className="container-fluid">
-	<div className="container-fluid header-dash valign-wrapper">
-		<div className="row">
-			<div className="col s12">
-				<p className="welcome-text">Welcome</p>
-			</div>
-		</div>
-	</div>
+	componentWillMount() {
+		this.setState({ profile: {} });
+		const { userProfile, getProfile } = this.props.auth;
+		if (!userProfile) {
+			getProfile((err, profile) => {
+				this.setState({ profile: profile });
+			});
+		} else {
+			this.setState({ profile: userProfile });
+		}
+	}
+	render() {
+		const { profile, email } = this.state;
+		return (
+			<div className="container-fluid">
+				<div className="container-fluid header-dash valign-wrapper">
+					<div className="row">
+						<div className="col s12">
+							<p className="welcome-text">Welcome</p>
+						</div>
+					</div>
+				</div>
 
-	<div className="container all-padding">
-		<div className="row">
-			<div className="col s12">
-				<center><h2><ProfileName profile={profile} /></h2></center>
-			</div>
-		</div>
-		<div className="row">
-		<div className="col s12">
-			<div className="row">
-				<DashPanel>
-					<div className="right">
-						<ViewBtn />
+				<div className="container all-padding">
+					<div className="row">
+						<div className="col s12">
+							<center><h2><ProfileName profile={profile} /></h2></center>
+						</div>
 					</div>
-					<DashPanelName />
-				</DashPanel>
-			</div>
-			<div className="row">
-				<DashPanel>
-					<div className="right">
-						<ViewBtn />
+					<div className="row">
+						<div className="col s12">
+							<div className="row">
+								<DashPanel>
+									<div className="right">
+										<ViewBtn />
+									</div>
+									<DashPanelName />
+								</DashPanel>
+							</div>
+							<div className="row">
+								<DashPanel>
+									<div className="right">
+										<ViewBtn />
+									</div>
+									<DashPanelName />
+								</ DashPanel>
+							</div>
+							<div className="row">
+								<AddBtnAlt />
+							</div>
+						</div>
 					</div>
-					<DashPanelName />
-					</ DashPanel>
+				</div>
 			</div>
-			<div className="row">
-				<AddBtnAlt />
-			</div>
-		</div>
-	</div>
-	</div>
-</div>
-        );
-    }
+		);
+	}
 }
 export default Dashboard;
