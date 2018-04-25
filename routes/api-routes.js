de78384bb1490282fcdc697170dda266b855e954
Erik McDaniel <prankq@gmail.com>
2018-04-24 20:11:24 -0600

    cleaned up api-routes adding in comments

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,7 +3,8 @@
 // *********************************************************************************
 
 var db = require("../models");
-
+// ROUTE SCHEMA
+// -----------------------
 // GET    /<model>     - get all <model>s
 // POST   /<model>     - create a <model>
 // GET    /<model>/:id - get a <model>
@@ -16,69 +17,120 @@ var db = require("../models");
 // *********************************************************************************
 
 module.exports = function(app) {
-  app.get('/api/budget/:id/income_exp/', function(req, res) {
-    // get all income exps for current user
-    db.IncomeExps.findAll({
-      where: {
-        Budget_id: req.params.id
-      }
-    }).then(function(dbListIE) {
-      res.json(dbListIE);
+  // -----------------------
+  // Budgets
+  // -----------------------
+  // creates a new budget
+      app.post('/api/budget', function(req, res) {
+        db.Budgets.create({
+          name: req.body.name
+          // user_email: req.body.user_email
+        }).then(function(dbBudget) {
+          res.json(dbBudget);
+        });
+      });
+  // returns budget object from budget id
+      app.get('/api/budget/:id', function(req, res) {
+        db.Budgets.findOne({
+          where: {
+            id: req.params.id
+          }
+        }).then(function(dbBudget) {
+          res.json(dbBudget);
+        });
+      })
+  // return budgets belonging to user_email
+    app.get('/api/user/:email', function(req, res) {
+      db.User.findOne({
+        where: {
+          email: req.params.email
+        }
+      }).then(function(dbUser) {
+        res.json(dbUser);
+      });
     });
-  });
+  // update budget with budget id
+      app.put('/api/budget/:id', function(req, res) {
 
-  // get route that returns budget object from budget id
-  app.get('/api/budget/:id', function(req, res) {
-    db.Budgets.findOne({
-      where: {
-        id: req.params.id
-      }
-    }).then(function(dbBudget) {
-      res.json(dbBudget);
-    });
-  })
+      });
+  // delete budget with budget id
+      app.delete('/api/budget/:id', function(req, res) {
 
-  app.get('/api/budget/:budget_id/income_exp/:income_exp_id', function(req, res) {
-    db.IncomeExps.findOne({
-      where: {
-        Budget_id: req.params.budget_id,
-        id: req.params.income_exp_id
-      }
-    }).then(function(dbIncomeExp) {
-      res.json(dbIncomeExp);
-    });
-  })
+      });
+  // -----------------------
+  // BudgetItems (IncomeExp)
+  // -----------------------
+  // returns all income-expense items
+      app.get('/api/budget/:id/income_exp/', function(req, res) {
+        db.IncomeExps.findAll({
+          where: {
+            Budget_id: req.params.id
+          }
+        }).then(function(dbListIE) {
+          res.json(dbListIE);
+        });
+      });
+  // returns income-expense object with income-expense id
+      app.get('/api/budget/:budget_id/income_exp/:income_exp_id', function(req, res) {
+        db.IncomeExps.findOne({
+          where: {
+            Budget_id: req.params.budget_id,
+            id: req.params.income_exp_id
+          }
+        }).then(function(dbIncomeExp) {
+          res.json(dbIncomeExp);
+        });
+      })
+  // create income-expense item
+      app.post('/api/budget/:id/income_exp', function(req, res) {
+        db.IncomeExps.create({
+          Budget_id: req.params.id,
+          title: req.body.title,
+          type: req.body.type,
+          value: req.body.value,
+          // date: req.body.date,
+          // frequency: req.body.frequency,
+          // willRecur: req.body.willRecur
+        }).then(function(dbUser) {
+          res.json(dbUser);
+        });
+      });
+  // update income-expense with income-expense id
+      app.put('/api/income_exp/:id', function(req, res) {
 
-  // creating budget object
-  app.post('/api/budget', function(req, res) {
-    db.Budgets.create({name: req.body.name}).then(function(dbBudget) {
-      res.json(dbBudget);
-    });
-  });
+      });
+  // delete income-expense with income-expense id
+      app.delete('/api/income_exp/:id', function(req, res) {
 
-  app.post('/api/budget/:id/income_exp', function(req, res) {
-    // create income exp model
-    db.IncomeExps.create({Budget_id: req.params.id, title: req.body.title, type: req.body.type, value: req.body.value}).then(function(dbUser) {
-      res.json(dbUser);
-    });
-  });
+      });
 
-  // get route that returns user object from user email
-  app.get('/api/user/:email', function(req, res) {
-    db.User.findOne({
-      where: {
-        email: req.params.email
-      }
-    }).then(function(dbUser) {
-      res.json(dbUser);
-    });
-  });
+  // -----------------------
+  // Users
+  // -----------------------
+  // GET /api/users/
+  //  returns User's id and email
+  //
+  // POST /api/users/
+  //  creates a new user
+  //
+  // GET /api/users/:email
+  //  returns user object from user email
+      // app.get('/api/user/:email', function(req, res) {
+      //   db.User.findOne({
+      //     where: {
+      //       email: req.params.email
+      //     }
+      //   }).then(function(dbUser) {
+      //     res.json(dbUser);
+      //   });
+      // });
+  //
+  // GET /api/users/:id
+  //  returns User's email
 
-  app.put('/api/income_exp/:id', function(req, res) {
-    // update income exp model
-  });
 
-  app.delete('/api/income_exp/:id', function(req, res) {
-    // delete income exp model
-  });
+
+
+
+
 };
