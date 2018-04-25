e5c0f87fffe5e58e489402c5e62c8761f1f15252
Jordan Boggs <boggsjp@gmail.com>
2018-04-24 20:43:44 -0600

    fixed auth0 callback

--- client/src/Auth/auth0-variables.js
+++ client/src/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: 'moneyhoney.auth0.com',
   clientId: 'utaTkWtFPulMplD6oAf71dp2gLyI8o2W',
-  callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: 'https://moneyhoneyapp.herokuapp.com/callback'
 }
