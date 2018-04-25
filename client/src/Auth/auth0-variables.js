e641df3344ad4a3f35b5b2e32061cdb9ca298e6b
Chaucee <hi@chaucee.com>
2018-04-25 09:12:48 -0600

    added animations to dashboard

--- client/src/Auth/auth0-variables.js
+++ client/src/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: 'moneyhoney.auth0.com',
   clientId: 'utaTkWtFPulMplD6oAf71dp2gLyI8o2W',
-  callbackUrl: 'https://moneyhoneyapp.herokuapp.com/callback'
+  callbackUrl: 'http://localhost:3000/callback'
 }
